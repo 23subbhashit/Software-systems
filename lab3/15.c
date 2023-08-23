9cf0b854b9e0c77a288d002fc5f8b5b7782866a2
subbhashit <subbhashitmukherjee@gmail.com>
2023-08-23 07:23:04 -0700

    15p

--- lab3/15.c
+++ lab3/15.c
@@ -1,40 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/select.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main() {
-    printf("Waiting for input from STDIN for 10 seconds...\n");
-    
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(STDIN_FILENO, &readfds);
-    
-    struct timeval timeout;
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
-    
-    int select_result = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &timeout);
-    
-    if (select_result == -1) {
-        perror("Error in select");
-        return 1;
-    }
-    
-    if (select_result == 0) {
-        printf("Timeout reached, no data received within 10 seconds.\n");
-    } else {
-        if (FD_ISSET(STDIN_FILENO, &readfds)) {
-            printf("Data is available within 10 seconds!\n");
-            char input[256];
-            if (fgets(input, sizeof(input), stdin)) {
-                input[strcspn(input, "\n")] = '\0'; // Remove newline character
-                printf("You entered: %s\n", input);
-            }
+extern char **environ;
+int prefixMatch(const char *pre, const char *str){
+    return strncmp(pre,str,strlen(pre))==0;
+}
+int main(){
+    char **env = environ;
+    while (*env!=NULL){
+        if(prefixMatch("USER",*env)){
+            printf("%s\n",*env);
+
         }
+        env++;
     }
-    
     return 0;
 }
