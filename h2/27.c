a97b4ddefb8ce40890e20c28f245f23491655353
subbhashit <subbhashitmukherjee@gmail.com>
2023-10-11 11:14:37 -0700

    27th header mistake update

--- h2/27.c
+++ h2/27.c
@@ -5,6 +5,7 @@ Author : Subbhashit Mukherjee
 Description : 
          Write a program to receive messages from the message queue.
             a. with 0 as a flag
+            b. with IPC_NOWAIT as a flag
 Date: 2nd Octuber 2023.
 ============================================================================
 */
